07352b6a790a651d7108c3d63819d1e32a5dc202
Vinu Baraguru <vinu.baraguru89@gmail.com>
2016-12-09 19:48:41 +0530

    Added Hiring module.

---
+++ app/src/main/java/android/vinu/com/epoiserecruiter/model/HiringCandidateList.java
@@ -0,0 +1,12 @@
+package android.vinu.com.epoiserecruiter.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by ePoise on 12/9/2016.
+ */
+
+public class HiringCandidateList implements Serializable {
+
+    private String appliedCandidateName;
+}
